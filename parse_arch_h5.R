f5c40eeb8adb4f06cfc3e69b5344a962f03ec4c7
aaronmaloney <124636643+aaronmaloney@users.noreply.github.com>
2023-07-28 09:45:37 -0700

    Update parse_arch_h5.R
    update for geoquerys and error handling

--- parse_arch_h5.R
+++ parse_arch_h5.R
@@ -1,6 +1,7 @@
 library("rhdf5")
 library(GEOquery)
 
+suppressWarnings({
 recounts_df <- read.csv("recount_overlap.csv")
 len_recounts <-1:length(recounts_df$organism)
 updated_df <- read.csv("UPDATED_overlap.csv")
@@ -9,15 +10,6 @@ len_updated <-1:length(updated_df$Dataset_ID)
 destination_file_human = "human_gene_v2.2.h5"
 destination_file_mouse = "mouse_gene_v2.2.h5"
 
-# #extracted_expression_file = "GSM2679484_expression_matrix.tsv"
-# url = "https://altos-lab-data-external.s3.us-west-2.amazonaws.com/ARCHS4_20235301630/human_gene_v2.2.h5"
-# 
-# # Check if gene expression file was already downloaded, if not in current directory download file form repository
-# if(!file.exists(destination_file)){
-#   print("Downloading compressed gene expression matrix.")
-#   download.file(url, destination_file, quiet = FALSE, mode = 'wb')
-# }
-#gse id, organisms , tissue type, and study title
 gse <- c()
 organism <- c()
 tissue_type <- c()
@@ -27,7 +19,8 @@ overlap_gse <-c()
 
 
 for (ind in len_updated)
-  if (updated_df$overlap_with_archs4[ind] != "Y"){
+  if (updated_df$overlap_with_archs4[ind] != "Y"){ 
+    print(ind)
     title_organisms <- c()
     if (grepl("human", updated_df$Source[ind], ignore.case=TRUE)){
       title_organisms <- append(title_organisms, "human")
@@ -54,7 +47,7 @@ for (ind in len_updated)
     tissue_type <-append(tissue_type, updated_df$Organ_specific[ind])
     study_title <-append(study_title, updated_df$Source[ind])
     dataset <- append(dataset, "Geneformer")
-    
+  }
 
 for (ind in len_recounts)
   if (recounts_df$overlap_with_archs4[ind] != "Y"){
@@ -65,11 +58,11 @@ for (ind in len_recounts)
       gse <-append(gse, recounts_df$series_id[ind])
     }
     organism <-append(organism, recounts_df$organism[ind])
-    tissue_type <-append(tissue_type, "")
+    tissue_type <- append(tissue_type,"")
     study_title <-append(study_title, recounts_df$study_title[ind])
     dataset <- append(dataset, "recount3")
   }
-  }
+  
     
 
 non_overlap_recount_updated_df  <- data.frame("repository_id"=gse,
@@ -78,7 +71,9 @@ non_overlap_recount_updated_df  <- data.frame("repository_id"=gse,
                                    "study title" = study_title,
                                    "dataset" = dataset)
 
+write.csv(non_overlap_recount_updated_df,"recount3_geneformer.csv")
 
+recount3_genformer_df <- read.csv("recount3_geneformer.csv")
   
 # Retrieve information from compressed data
 human_gse = h5read(destination_file_human, "meta/samples/series_id")
@@ -92,44 +87,61 @@ archs_sample <- c()
 archs_title <- c()
 archs_dataset <- c()
 
-for (ind in 1:length(human_gse))
-  archs_gse <- append(archs_gse, human_gse[ind])
-  archs_organism <- append(archs_organism, "human")
-  archs_sample <- append(archs_sample,human_source[ind])
-  gse_ind <- getGEO(GEO = human_gse[ind], destdir = "./",
-                GSEMatrix = FALSE)
-  archs_title <-append(archs_title, Meta(gse_ind)[c("title")])
-  archs_dataset <-append(archs_dataset, "archs4")
+find_title <- function(i) {
+  tryCatch(
+    {
+      gse_ind <- getGEO(GEO = i, destdir = "./", GSEMatrix = FALSE)
+      return(Meta(gse_ind)[c("title")]$title)
+    },
+    error=function(e) {
+      message('An Error Occurred')
+      return("")
+    },
+    warning=function(w) {
+      message('A Warning Occurred')
+      return("")
+    }
+  )
+}
+
+
+
+for (ind in 1:length(human_gse)){
+  if (grepl(",", human_gse[ind], ignore.case=TRUE)){
+    gse_list <- strsplit(human_gse[ind], split = ",")
+    human_gse[ind] = gse_list[[1]]
+  }
+  if (!(human_gse[ind] %in% archs_gse)){
+    archs_gse <- append(archs_gse, human_gse[ind])
+    archs_organism <- append(archs_organism, "human")
+    archs_sample <- append(archs_sample,human_source[ind])
+    archs_title <-append(archs_title,  find_title(human_gse[ind]))
+    archs_dataset <-append(archs_dataset, "archs4")
+  }}
+
   
-for (ind in 1:length(mouse_gse))
-  archs_gse <- append(archs_gse, mouse_gse[ind])
-  archs_organism <- append(archs_organism, "mouse")
-  archs_sample <- append(archs_sample,mouse_source[ind])
-  gse_ind <- getGEO(GEO = mouse_gse[gse_ind], destdir = "./",
-                GSEMatrix = FALSE)
-  archs_title <-append(archs_title, Meta(gse_ind)[c("title")])
-  archs_dataset <-append(archs_dataset, "archs4")
+for (ind in 1:length(mouse_gse)){
+  if (grepl(",", mouse_gse[ind], ignore.case=TRUE)){
+    gse_list <- strsplit(mouse_gse[ind], split = ",")
+    mouse_gse[ind] = gse_list[[1]]
+  }
+  if (!(mouse_gse[ind] %in% archs_gse)){
+    archs_gse <- append(archs_gse, mouse_gse[ind])
+    archs_organism <- append(archs_organism, "mouse")
+    archs_sample <- append(archs_sample,mouse_source[ind])
+    archs_title <-append(archs_title,  find_title(mouse_gse[ind]))
+    archs_dataset <-append(archs_dataset, "archs4")
+    
+  }}
+
+
   
-  
-final_df <-  data.frame("repository_id"=c(gse, archs_gse),
-                        "organism" = c(organism, archs_organism),
-                        "tissue_type" = c(tissue_type, archs_sample),
-                        "study title" = c(study_title, archs_title),
-                        "dataset" = c(dataset, archs_dataset))
+final_df <-  data.frame("repository_id"=c(recount3_genformer_df$repository_id, archs_gse),
+                        "organism" = c(recount3_genformer_df$organism, archs_organism),
+                        "tissue_type" = c(recount3_genformer_df$tissue_type, archs_sample),
+                        "study_title" = c(recount3_genformer_df$study_title, archs_title),
+                        "dataset" = c(recount3_genformer_df$dataset, archs_dataset))
 
 write.csv(final_df,"archs4_recount3_geneformer.csv")
 
-# for (ind in len_series)
-#     if ((organism[ind] == "human") && ( recount_gse[ind] != "") && (recount_gse[ind] %in% human_gse)){
-#         overlap <- append(overlap,"Y")
-#         } else if ((organism[ind] == "mouse") && ( recount_gse[ind] != "") && (recount_gse[ind] %in% mouse_gse)) {
-#         overlap <- append(overlap,"Y")
-#         } else {
-#         overlap <- append(overlap,"")
-#     }
-# 
-# 
-# 
-# recounts_df$overlap_with_archs4 = overlap
-# 
-# write.csv(recounts_df,"UPDATED_overlap.csv")
+})
